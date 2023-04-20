a8e0262c0ded7117fb217ddba0165002d9889d41
Admin <Admin@DESKTOP-9L5JQOO>
2023-04-20 17:19:28 +0800

    Add project files.

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Employee
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Department { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // List of Employee (as objects)
+        List<Employee> employees = new List<Employee>
+        {
+            new Employee { Name = "John", Age = 25, Department = "HR" },
+            new Employee { Name = "Jane", Age = 30, Department = "Finance" },
+            new Employee { Name = "Bob", Age = 35, Department = "IT" },
+            new Employee { Name = "Alice", Age = 40, Department = "Finance" },
+        };
+
+        // Lambda Expression
+        var filteredEmployees = employees.Where(emp => emp.Age > 30 && emp.Department == "Finance").ToList();
+
+        
+        foreach (var employee in filteredEmployees)
+        {
+            
+            Console.WriteLine($"Name: {employee.Name}, Age: {employee.Age}, Department: {employee.Department}");
+
+            
+            if (employee.Name.StartsWith("J"))
+            {
+                Console.WriteLine("Name starts with 'J'");
+            }
+
+            if (employee.Age > 35)
+            {
+                Console.WriteLine("Age is greater than 35");
+            }
+
+            if (employee.Department == "Finance")
+            {
+                Console.WriteLine("Department is Finance");
+            }
+
+            // You can add more conditions
+        }
+
+        Console.ReadLine();
+    }
+}
